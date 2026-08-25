bc5fc2c7921ff406edd3d20ccfae56ef60e53b56
raghavluthra555-eng <raghavluthra555@gmail.com>
2026-08-25 15:14:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    nitro({ preset: "vercel" }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
